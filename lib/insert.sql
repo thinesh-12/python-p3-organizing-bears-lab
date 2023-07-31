7f1456aa1ad271f470f6c0dcb88f7402026b9c58
= <=>
2023-07-30 23:32:56 -0400

    completed lab

---
+++ lib/insert.sql
@@ -0,0 +1,39 @@
+INSERT INTO bears
+(name, age, sex, color, temperament, alive)
+VALUES 
+('Mr.Chocolate', 2, 'M', 'Brown', 'chill', 0)
+
+INSERT INTO bears
+(name, age, sex, color, temperament, alive)
+VALUES
+('Rowdy', 1, 'M', 'Black', 'rowdy', 1);
+
+INSERT INTO bears
+(name, age, sex, color, temperament, alive)
+VALUES
+('Tabitha', 4, 'F', 'Blue', 'helpful', 1);
+
+INSERT INTO bears
+(name, age, sex, color, temperament, alive)
+VALUES
+('Sergeant Brown', 10, 'M', 'Grey', 'strict', 0);
+
+INSERT INTO bears
+(name, age, sex, color, temperament, alive)
+VALUES
+('Melissa', 15, 'F', 'Purple', 'angry', 1);
+
+INSERT INTO bears
+(name, age, sex, color, temperament, alive)
+VALUES
+('Grinch', 7, 'M', 'Green', 'selfish', 1);
+
+INSERT INTO bears
+(name, age, sex, color, temperament, alive)
+VALUES
+('Wendy', 4, 'F', 'Red', 'loner', 1);
+
+INSERT INTO bears
+(name, age, sex, color, temperament, alive)
+VALUES
+(NULL, 1, 'M', 'Orange', 'quiet', 0);
